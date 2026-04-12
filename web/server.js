20ffa263059d343252575ae9e42fa059c0e031b3
Sall-lah <ikhsan.ibnu.abdulah@gmail.com>
2026-04-12 16:55:56 +0700

    fix(01-03): update Express routing to support v5 path-to-regexp

--- web/server.js
+++ web/server.js
@@ -30,7 +30,7 @@ app.use(
 app.use(express.static(path.join(__dirname, 'dist')));
 
 // Fallback to index.html for SPA routing
-app.get('*', (req, res) => {
+app.get(/(.*)/, (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
